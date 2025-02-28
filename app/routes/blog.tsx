cb2c99ecad841d8912259f1d27dfbd667f4b090b
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2025-02-28 13:39:32 +0100

    Initial blog - post view

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -14,18 +14,18 @@ export function meta({}: Route.MetaArgs) {
 export default function Blog() {
   return (
     <div className="w-page mx-auto flex flex-col my-4">
-      <div className="my-4 p-4 md:px-16">
+      <div className="py-2 px-4 md:py-4 md:my-4 md:px-16">
         <h1 className="font-garet text-3xl">Latest posts 📖</h1>
         <div className="flex flex-col gap-8 my-4">
-          <article className="grid grid-cols-3 gap-4 bg-background border rounded p-4 pr-8">
-            <div className="w-full border border-black">Test image</div>
+          <article className="flex flex-col md:grid grid-cols-3 gap-4 bg-background border rounded p-4 pr-8">
+            <div className="w-full h-16 border border-black">Test image</div>
             <div className="col-span-2 flex flex-col gap-2">
               <h3 className="text-sm flex gap-2">
                 <span>#javascript</span>
                 <span>#programming</span>
               </h3>
               <h2 className="text-2xl font-semibold pb-2">
-                <Link to="/blog" className="block hover:opacity-50">Test title for the blog</Link>
+                <Link to="/blog/test" className="block hover:opacity-50">Test title for the blog</Link>
               </h2>
               <div className="flex flex-col gap-2">
                 <p>
@@ -37,7 +37,7 @@ export default function Blog() {
                   time.
                 </p>
               </div>
-              <Link to="/blog" className="self-end font-semibold hover:opacity-50">Read more</Link>
+              <Link to="/blog/test" className="self-end font-semibold hover:opacity-50">Read more</Link>
             </div>
           </article>
         </div>
