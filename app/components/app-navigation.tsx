42628b67a0729f6c7423632e5c52784b2cc4d4aa
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2025-02-28 10:52:14 +0100

    Ensure navigation works well on mobiles

--- app/components/app-navigation.tsx
+++ app/components/app-navigation.tsx
@@ -4,10 +4,26 @@ import { ThemeToggle } from '@/components/theme-toggle';
 import { LinkedIn } from '@/components/linkedin';
 import { GitHub } from '@/components/github';
 import { SocialLink } from '@/components/social-link';
+import { MenuIcon, XIcon } from 'lucide-react';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
 
 type NavigationProps = {
   theme?: string;
 }
+
+const Links = () => (
+  <>
+    <Link to="/" className="hover:opacity-50">About me</Link>
+  </>
+)
 
 export const AppNavigation = ({ theme }: NavigationProps) => (
   <nav className="border-b flex justify-center">
@@ -17,9 +33,42 @@ export const AppNavigation = ({ theme }: NavigationProps) => (
           <img src="/logo_simple.png" className="size-8 md:size-12" alt="Home"/>
           <span className="text-xxs">megawebmaster</span>
         </Link>
-        <Link to="/" className="hover:opacity-50">About me</Link>
+        <div className="hidden md:block">
+          <Links />
+        </div>
       </div>
-      <div className="flex gap-4 items-center">
+      <Drawer direction="right">
+        <DrawerTrigger className="md:hidden">
+          <MenuIcon />
+          <span className="sr-only">Open menu</span>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader className="flex flex-row items-center">
+            <DrawerTitle className="grow">Navigation</DrawerTitle>
+            <DrawerClose>
+              <XIcon />
+            </DrawerClose>
+          </DrawerHeader>
+          <div className="px-8 pb-8 flex flex-col gap-4">
+            <Links />
+          </div>
+          <h3 className="font-semibold px-4 pb-4">Socials</h3>
+          <div className="px-8 flex flex-col gap-3">
+            <SocialLink href="https://linkedin.com/in/amadeusz-starzykiewicz">
+              <LinkedIn className="size-6"/>
+              <span>LinkedIn</span>
+            </SocialLink>
+            <SocialLink href="https://github.com/megawebmaster">
+              <GitHub className="size-6"/>
+              <span>GitHub</span>
+            </SocialLink>
+          </div>
+          <DrawerFooter>
+            <ThemeToggle defaultTheme={theme} className="self-center"/>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+      <div className="hidden gap-4 items-center md:flex">
         <ThemeToggle defaultTheme={theme} className="mr-4 hidden md:flex"/>
         <SocialLink href="https://linkedin.com/in/amadeusz-starzykiewicz">
           <LinkedIn className="size-6"/>
--- app/components/social-link.tsx
+++ app/components/social-link.tsx
@@ -1,12 +1,12 @@
 import type { JSX } from 'react';
 
 type SocialLinkProps = {
-  children: JSX.Element;
+  children: JSX.Element | JSX.Element[];
   href: string;
 }
 
 export const SocialLink = ({ children, href }: SocialLinkProps) => (
-  <a className="hover:opacity-50" target="_blank" href={href}>
+  <a className="hover:opacity-50 flex items-center gap-2" target="_blank" href={href}>
     {children}
   </a>
 );
