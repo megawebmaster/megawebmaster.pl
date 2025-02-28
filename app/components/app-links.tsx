298acdad13387a95e09ac83f9f8e17dd3cd94650
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2025-02-28 13:31:42 +0100

    Initial blog - posts list view

--- app/components/app-links.tsx
+++ app/components/app-links.tsx
@@ -7,5 +7,6 @@ type LinksProps = {
 export const AppLinks = ({ onClick }: LinksProps) => (
   <>
     <Link to="/" className="hover:opacity-50" onClick={onClick}>About me</Link>
+    <Link to="/blog" className="hover:opacity-50" onClick={onClick}>Blog</Link>
   </>
 );
