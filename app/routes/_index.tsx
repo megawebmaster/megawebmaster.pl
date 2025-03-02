672bafff40f7c4f0429575b18ae8f5a7b77bfb8f
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2025-03-02 21:39:18 +0100

    Simplify a bit structure of index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,19 +10,15 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Index() {
   return (
-    <div className="w-page mx-auto flex flex-col my-4">
-      <div className="relative">
-        <div className="absolute top-0 right-0 z-0">
-          <picture>
-            <source type="image/webp" srcSet="/amadeusz.webp, /amadeusz@2x.webp 2x"/>
-            <source type="image/png" srcSet="/amadeusz.png, /amadeusz@2x.png 2x"/>
-            <img src="/amadeusz.png" alt="Amadeusz's picture" className="block hero-image"/>
-          </picture>
-        </div>
-        <div className="relative z-10 my-4 p-8 pt-56 md:my-8 md:p-16 md:pr-52">
-          <AboutMe />
-        </div>
+    <div className="w-page mx-auto flex flex-col relative">
+      <div className="absolute top-4 right-0 z-0">
+        <picture>
+          <source type="image/webp" srcSet="/amadeusz.webp, /amadeusz@2x.webp 2x"/>
+          <source type="image/png" srcSet="/amadeusz.png, /amadeusz@2x.png 2x"/>
+          <img src="/amadeusz.png" alt="Amadeusz's picture" className="block hero-image"/>
+        </picture>
       </div>
+      <AboutMe/>
     </div>
   );
 }
