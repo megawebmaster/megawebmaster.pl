256d59045c5e54a063d9839f4776c67ec962f837
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2025-03-03 00:04:49 +0100

    Add active link classes

--- app/components/app-links.tsx
+++ app/components/app-links.tsx
@@ -1,4 +1,23 @@
-import { Link } from 'react-router';
+import type { ReactNode } from 'react';
+import { NavLink } from 'react-router';
+
+
+type LinkProps = {
+  children: ReactNode;
+  onClick?: () => void;
+  to: string;
+}
+
+const Link = ({ children, onClick, to }: LinkProps) => (
+  <NavLink
+    className={({ isActive }) => 'py-1 px-2 hover:opacity-50' + (isActive ? ' border-b border-accent' : '')}
+    onClick={onClick}
+    to={to}
+    viewTransition
+  >
+    {children}
+  </NavLink>
+);
 
 type LinksProps = {
   onClick?: () => void;
@@ -6,7 +25,7 @@ type LinksProps = {
 
 export const AppLinks = ({ onClick }: LinksProps) => (
   <>
-    <Link to="/" className="hover:opacity-50" onClick={onClick}>About me</Link>
-    <Link to="/resume" className="hover:opacity-50" onClick={onClick}>Resume</Link>
+    <Link to="/" onClick={onClick}>About me</Link>
+    <Link to="/resume" onClick={onClick}>Resume</Link>
   </>
 );
