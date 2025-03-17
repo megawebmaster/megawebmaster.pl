c93aa87f165e3379b3296f1fb147d4cf8b24a2f8
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2025-03-17 13:56:09 +0100

    Improve spacing around paragraphs

--- app/components/mdx/p.tsx
+++ app/components/mdx/p.tsx
@@ -1,3 +1,3 @@
 import type { ReactNode } from 'react';
 
-export default ({ children }: { children: ReactNode }) => <p className="my-2">{children}</p>;
+export default ({ children }: { children: ReactNode }) => <p className="my-3">{children}</p>;
