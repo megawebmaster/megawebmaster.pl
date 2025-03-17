5140f8defcbfabaebcaeeecf1280b6652b94fc41
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2025-03-17 14:39:39 +0100

    Import components in MDX explicitly

--- app/components/mdx/p.tsx
+++ app/components/mdx/p.tsx
@@ -1,3 +1,3 @@
 import type { ReactNode } from 'react';
 
-export default ({ children }: { children: ReactNode }) => <p className="my-3">{children}</p>;
+export default ({ children }: { children: ReactNode }) => <p className="my-3 first:mt-0 last:mb-0">{children}</p>;
